d50c84ac2b8b67ddd20dada9994228f6ef909240
OkothPius <orukopius8@gmail.com>
2019-02-15 08:32:47 +0300

    making input1 a string

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function triangletracker(){
-  var sideA=parseInt(document.getElementById(input1).value);
+  var sideA=parseInt(document.getElementById('input1').value);
   var sideB=parseInt(document.getElementById(input2).value);
   var sideC=parseInt(document.getElementById(input3).value);
   if (sideA ===sideB && sideB ===sideC) {
