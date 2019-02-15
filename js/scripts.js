4f0d6564186650c34d81441b84557522a9d9854e
OkothPius <orukopius8@gmail.com>
2019-02-15 16:14:14 +0300

    Adding not a triangle condition

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,17 @@ function triangleTracker(){
   var sideA=parseInt(document.getElementById('inputOne').value);
   var sideB=parseInt(document.getElementById('inputTwo').value);
   var sideC=parseInt(document.getElementById('inputThree').value);
+  if((sideA + sideB >sideC) && (sideA + sideC > sideB) && (sideB + sideC > sideA)){
 if (sideA === sideB && sideB === sideC) {
   alert("This is an equilateral triangle");
 }
 else if (sideA === sideB || sideA === sideC || sideC === sideB) {
   alert("This is an isosceles triangle");
 }
-  else if (sideA != sideB != sideC) {
+  else {
     alert("This is a scalene triangle");
   }
+}
   else {
     alert("A triangle cannot be formed!");
 }
