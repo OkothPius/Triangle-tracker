fb5255ba7d428e26f8d40eef497ec189a54381f3
OkothPius <orukopius8@gmail.com>
2019-02-15 08:08:03 +0300

    changing iput three to integer

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 function triangletracker(){
   sideA=parseInt(document.getElementById(input1).value);
   sideB=parseInt(document.getElementById(input2).value);
+  sideC=parseInt(document.getElementById(input3).value);
 }
